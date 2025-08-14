93818c9b24209c85ef1e6e6746304d170b52c7bb
dakian <dakiandang@gmail.com>
2025-08-14 18:34:52 +0700

    update file readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Blog-Fullstack
-Project to seriously  learn fullstack
+# News-Fullstack
+
+Project to seriously learn fullstack
+
+Backend:
+
+- User model, controller checked
+- News model, controller checked
+- Middleware auth checked
+- Validation + ErrorHandler empty (Do later)
+
+Frontend:
+
+- Must insert some data about news into database!
+
+* Regular pages:
+
+- Homepage (Do after login, logout, register)
+- List News (Filter)
+- News detail
+
+* Admin pages:
+
+- Login (Do now)
+- Logout (Do now)
+- Register (Do now)
+- Create/Edit/Update news
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -24,7 +24,7 @@ const startServer = async () => {
 
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
-    console.log("http://localhost:5000/");
+    console.log("http://localhost:5000");
   } catch (error) {
     console.error("🔥 Database connection failed:", error);
     process.exit(1);
@@ -36,19 +36,18 @@ startServer();
 // Login and Logout user
 app.post("/login", async (req, res) => {
   const sessionToken: string = await generateSessionToken(); // e.g., JWT
-  
+
   res.cookie("authorization", sessionToken, {
-    httpOnly: true,     // Can't be accessed by JS (XSS protection)
-    secure: true,       // Cookie only sent over HTTPS
+    httpOnly: true, // Can't be accessed by JS (XSS protection)
+    secure: true, // Cookie only sent over HTTPS
     sameSite: "strict", // Prevent CSRF
   });
-  
+
   // Login user
   await loginUser(sessionToken, req, res);
 
   res.json({ message: "Logged in" });
 });
-
 
 app.post("/logout", async (req, res) => {
   // Logout user
@@ -58,4 +57,3 @@ app.post("/logout", async (req, res) => {
   res.clearCookie("authorization");
   res.json({ message: "Logged out" });
 });
-
